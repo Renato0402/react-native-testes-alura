9d06364d8347f54d05b974f588ad4cdbdcd76877
Renato Vidal Moura <50018235+Renato0402@users.noreply.github.com>
2023-02-26 23:51:39 -0300

    Testes com mocks

---
+++ src/__tests__/negocio/repositorio/leilao.test.js
@@ -0,0 +1,58 @@
+import { obtemLeiloes } from "../../../../src/repositorio/leilao";
+import apiLeiloes from "../../../../src/servicos/apiLeiloes";
+
+jest.mock("../../../../src/servicos/apiLeiloes");
+
+const mockLeiloes = [
+  {
+    id: 1,
+    nome: "Leilao",
+    descricao: "Descricao do leilao",
+  },
+];
+
+const mockRequisicao = (retorno) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: retorno,
+      });
+    }, 200);
+  });
+};
+
+const mockRequisicaoErro = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 200);
+  });
+};
+
+describe("respositorio/leilao", () => {
+  beforeEach(() => {
+    apiLeiloes.get.mockClear();
+  });
+
+  describe("obtemLeiloes", () => {
+    it("deve retornar uma lista de leiloes", async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes));
+      const leiloes = await obtemLeiloes();
+      expect(leiloes).toEqual(mockLeiloes);
+
+      expect(apiLeiloes.get).toHaveBeenCalledWith("/leiloes");
+
+      expect(apiLeiloes.get).toHaveBeenCalledTimes(1);
+    });
+
+    it("deve retornar uma lista vazia quando a requisicao falhar", async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicaoErro());
+      const leiloes = await obtemLeiloes();
+      expect(leiloes).toEqual([]);
+
+      expect(apiLeiloes.get).toHaveBeenCalledWith("/leiloes");
+
+      expect(apiLeiloes.get).toHaveBeenCalledTimes(1);
+    });
+  });
+});
